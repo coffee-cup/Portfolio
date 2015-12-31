e34d542a26238b9093599c68d1dd26723ccafb50
Jake Runzer <jakerunzer@gmail.com>
2015-12-31 02:35:35 -0700

    contact update

--- views/Contact.vue
+++ views/Contact.vue
@@ -1,29 +1,52 @@
 <template>
 
   <div class="contact row-section c100">
-    <div class="s100">
+    <div class="s100 tcenter">
 
+      <a class='no-under' href="mailto:jakerunzer@gmail.com">
       <div class="contact-row c80r pr">
-        <span class="icomoon-email"></span>
-        <span class="contact-title">Send me an email</span>
-      </div>
-
-      <div class="contact-row c80r pr">
-        <span class="contact-icon">
-          <span class="icomoon-github"></span>
+        <span class="s100">
+          <h1 class='mt0 mb0'>
+            <span class="icomoon-email"></span>
+          </h1>
         </span>
-        <span class="contact-title">Check out my projects</span>
+        <h3 class="s100 light mt0 contact-title">Send me an email</h3>
       </div>
+      </a>
 
+      <a class='no-under' href="https://github.com/coffee-cup">
       <div class="contact-row c80r pr">
-        <span class="icomoon-twitter"></span>
-        <span class="contact-title">Follow me on Twitter</span>
+        <span class="s100">
+          <h1 class='mt0 mb0'>
+            <span class="icomoon-github"></span>
+          </h1>
+        </span>
+        <h3 class="s100 light mt0 contact-title">Check out my projects</h3>
       </div>
+      </a>
 
+      <a class='no-under' href="https://twitter.com/jakerunzer">
       <div class="contact-row c80r pr">
-        <span class="icomoon-linkedin"></span>
-        <span class="contact-title">Connect on LinkedIn</span>
+        <span class="s100">
+          <h1 class='mt0 mb0'>
+            <span class="icomoon-twitter"></span>
+          </h1>
+        </span>
+        <h3 class="s100 light mt0 contact-title">Follow me on Twitter</h3>
       </div>
+      </a>
+
+      <a class='no-under' href="https://ca.linkedin.com/in/jakerunzer
+">
+       <div class="contact-row c80r pr">
+        <span class="s100">
+          <h1 class='mt0 mb0'>
+            <span class="icomoon-linkedin"></span>
+          </h1>
+        </span>
+        <h3 class="s100 light mt0 contact-title">Connect on LinkedIn</h3>
+      </div>
+      </a>
 
     </div>
   </div>
